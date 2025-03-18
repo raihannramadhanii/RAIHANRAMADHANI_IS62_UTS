df8feebecf70f02593c9bd41319cc7b667979758
raihannramadhanii <raihannramadhanii@gmail.com>
2025-03-18 17:10:57 +0700

    update pendaftaran.blade.php

--- resources/views/menu/pendaftaran.blade.php
+++ resources/views/menu/pendaftaran.blade.php
@@ -1,44 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    @yield('css')
-    <title>@yield('title', 'Pendaftaran Pengguna')</title>
-</head>
-<body>
+@extends('layout.master')
+@section('title')
+    Pendaftaran
+@endsection
+
+@section('pendaftaran', 'active')
+
+@section('konten')
 
 <div class="container mt-5">
     <h2 class="text-center mb-4">Form Pendaftaran</h2>
-    <form action="{{ route('register') }}" method="POST">
+    <form action="method="POST">
         @csrf
+
         <div class="form-group">
             <label for="name">Nama Lengkap</label>
             <input type="text" class="form-control" id="name" name="name" required>
         </div>
 
         <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <label for="email">Alamat</label>
+            <input type="email" class="form-control" id="alamat" name="alamat" required>
         </div>
 
         <div class="form-group">
-            <label for="password">Kata Sandi</label>
-            <input type="password" class="form-control" id="password" name="password" required>
-        </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">Konfirmasi Kata Sandi</label>
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+            <label for="password">No Hp</label>
+            <input type="text" class="form-control" id="hp" name="hp" required>
         </div>
 
         <button type="submit" class="btn btn-primary btn-block">Daftar</button>
     </form>
 </div>
 
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-@yield('scripts')
-</body>
-</html>
+@endsection
+
