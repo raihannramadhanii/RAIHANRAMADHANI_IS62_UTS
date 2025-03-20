263701a6d67e27cf280dd0dd3f7482d1cd9c3b27
raihannramadhanii <raihannramadhanii@gmail.com>
2025-03-20 13:06:35 +0700

    menghubungkan logout kembali ke ogin

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -42,7 +42,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-danger" href="Logout">
+                <a class="nav-link text-danger" href="login">
                   Logout
                 </a>
               </li>
