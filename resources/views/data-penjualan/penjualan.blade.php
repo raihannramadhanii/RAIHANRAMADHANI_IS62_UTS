b77e003160699145859fa4d2e9cfe4226b862c35
raihannramadhanii <raihannramadhanii@gmail.com>
2025-03-20 12:05:22 +0700

    membuat data penjualan di penjulan.blade.php

--- resources/views/data-penjualan/penjualan.blade.php
+++ resources/views/data-penjualan/penjualan.blade.php
@@ -32,35 +32,31 @@ Data Penjualan
             </tr>
             <tr>
                 <td>2</td>
+                <td>T30</td>
                 <td>86760</td>
+                <td>01/01/2025</td>
                 <td>30 Pcs</td>
-                <td>KB-T1</td>
-                <td>Tempe</td>
-                <td>Rp.2000,-</td>
             </tr>
             <tr>
                 <td>3</td>
+                <td>CG25</td>
                 <td>77545</td>
+                <td>01/01/2025</td>
                 <td>25 Pcs</td>
-                <td>CG</td>
-                <td>Canai Gula</td>
-                <td>Rp.2500,-</td>
             </tr>
             <tr>
                 <td>4</td>
+                <td>OND30</td>
                 <td>454455</td>
+                <td>02/02/2025</td>
                 <td>30 Pcs</td>
-                <td>OND</td>
-                <td>Onde-Onde</td>
-                <td>Rp.1000,-</td>
             </tr>
             <tr>
                 <td>5</td>
+                <td>PSC20</td>
                 <td>18787</td>
+                <td>02/02/2025</td>
                 <td>20 Pcs</td>
-                <td>PSC</td>
-                <td>Pisang Coklat</td>
-                <td>Rp.2000,-</td>
             </tr>
             <!-- Baris tambahan bisa ditambahkan di sini -->
         </tbody>
